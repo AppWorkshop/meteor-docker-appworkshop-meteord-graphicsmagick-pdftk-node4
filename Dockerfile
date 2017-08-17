64c172d94d543d72929378bbe4aeb47b3cc16dc4
Mike Cunneen <mike@appworkshop.net>
2017-08-17 18:00:02 +0800

    appworkshop/meteord-graphicsmagick-pdftk-node4 meteor docker image - first cut

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM abernix/meteord:base
+RUN apt-get update -y
+RUN apt-get install graphicsmagick -y
+RUN apt-get install pdftk -y
+RUN apt-get install wget -y
+RUN apt-get install build-essential chrpath libssl-dev libxft-dev -y
+RUN apt-get install libfreetype6 libfreetype6-dev -y
+RUN apt-get install libfontconfig1 libfontconfig1-dev -y
+ENV PHANTOM_JS phantomjs-2.1.1-linux-x86_64
+RUN wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
+RUN tar xvjf $PHANTOM_JS.tar.bz2
+RUN mv $PHANTOM_JS /usr/local/share
+RUN ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+RUN phantomjs --version
---
+++ build_new_meteord_image.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+dockerhubuser=appworkshop
+dockerimagename=meteord-graphicsmagick-pdftk-node4
+
+command -v jq >/dev/null 2>&1 || { echo "I require jq but it's not installed.  Aborting." >&2; exit 1; }
+
+echo building docker image
+docker build -t ${dockerimagename} .
+retval=$?
+if [ $retval -ne 0 ]; then
+   echo couldn\'t build docker image
+   exit $retval
+fi
+
+echo logging in to docker hub
+docker login
+retval=$?
+if [ $retval -ne 0 ]; then
+   echo couldn\'t login to docker hub
+   exit $retval
+fi
+
+echo tagging image as latest
+docker tag ${dockerimagename} ${dockerhubuser}/${dockerimagename}:latest
+retval=$?
+if [ $retval -ne 0 ]; then
+   echo couldn\'t tag docker image
+   exit $retval
+fi
+
+echo Existing tags for this image :
+curl -L -s "https://registry.hub.docker.com/v2/repositories/${dockerhubuser}/${dockerimagename}/tags?page_size=1024"|jq '."results"[]["name"]'
+
+echo create at least one new version tag for this image :
+read versiontag
+
+docker tag ${dockerimagename} "${dockerhubuser}/${dockerimagename}:${versiontag}"
+docker push ${dockerhubuser}/${dockerimagename}
